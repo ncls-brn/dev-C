f329e00caffc2c28c782922b114423d75ff305d9
ncls-brn <nicolas.j.baran@gmail.com>
2024-11-21 10:43:13 +0100

    " swap int in C "

--- programming.c
+++ programming.c
@@ -1,9 +1,28 @@
 #include <stdio.h> 
+void swap(int *a, int *b);
 
 
-int main(){
+int main(int argc, char const *argv[]){
 
-    printf("hello world!\n");
+    int a=1;
+    int b=2;
+    printf("a= %d\nb= %d\n",a,b );
+    printf("adresse memoire de a = %p\n", &a);
+    printf("adresse memoire de b = %p\n", &b);
+    swap(&a, &b);
+
+    printf("\n");
+    printf("a= %d\nb= %d\n",a,b );
+
 
     return 0;
+}
+
+
+void swap(int *a, int *b) {
+    printf("swapping\n");
+ int temp = *a;
+*a =*b; 
+*b =temp; 
+
 }
