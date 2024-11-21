0292ccef94c40f28a3b5dbad075fffbf99659374
ncls-brn <nicolas.j.baran@gmail.com>
2024-11-21 12:14:55 +0100

    " add toto values et adr and pointors "

--- say_hi.c
+++ say_hi.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
+struct Person{
+    int age ;
+    char name[5];
+    char* lastname;
+
+};
 
 
 int main(){
+    struct Person toto;
+    toto.age =33;
+    toto.lastname = "titi";
+    strcpy(toto.name, "toto");
 
-    printf("hello world\n");
+    struct Person* toto_add =&toto;
+
+    printf("age of person is: %d\n",toto.age);
+    printf("name is: %s\n", toto.name);
+    printf("lastname is: %s\n",toto.lastname);
+    printf(" the name is %s, the lastname is %s and he's %d years old.\n", toto.name, toto.lastname, toto.age);
+    printf("l'adresse est: %p\n",toto_add );
     return 0;
-}
-
-struct Personn{
-    int age ;
-    char* name;
-
 };
