e10fbeb0bbe0b4071eaeb22fc14ef13ad27d1c1f
ncls-brn <nicolas.j.baran@gmail.com>
2024-11-21 12:49:35 +0100

    " loop for with toto and titi "

--- say_hi.c
+++ say_hi.c
@@ -12,15 +12,39 @@ struct Person{
 int main(){
     struct Person toto;
     toto.age =33;
-    toto.lastname = "titi";
+    toto.lastname = "frfr";
     strcpy(toto.name, "toto");
 
     struct Person* toto_add =&toto;
+
+
+    struct Person titi;
+    titi.age = 25;
+    titi.lastname = "titi";
+    strcpy(titi.name, "titi");
+
+
+    for( int i= 0; i<12; i++){
+        printf("%c ", toto.name[i]);
+    }
+
+     for( int i= 0; i<12; i++){
+        printf("%c ", titi.name[i]);
+    }
+
+    printf("\n");
+
 
     printf("age of person is: %d\n",toto.age);
     printf("name is: %s\n", toto.name);
     printf("lastname is: %s\n",toto.lastname);
     printf(" the name is %s, the lastname is %s and he's %d years old.\n", toto.name, toto.lastname, toto.age);
     printf("l'adresse est: %p\n",toto_add );
+    printf("--------------------------");
+    printf("age of person is: %d\n",titi.age);
+    printf("name is: %s\n", titi.name);
+    printf("lastname is: %s\n",titi.lastname);
+    printf(" the name is %s, the lastname is %s and he's %d years old.\n", titi.name, titi.lastname, titi.age);
+
     return 0;
 };
