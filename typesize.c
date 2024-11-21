02b2040bcfbd1c23c3cb3726bf4158dca9c750b8
ncls-brn <nicolas.j.baran@gmail.com>
2024-11-21 15:25:20 +0100

    " realloc typesize"

--- typesize.c
+++ typesize.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 typedef struct Person {
@@ -9,5 +10,36 @@ typedef struct Person {
 
 
 int main(){
-    int numbers[5] ={1,2,3,4,5};
+
+    int size = 5;
+    int* numbers = malloc(size * sizeof(int));
+    if (numbers == NULL) {
+        return 1;
+    }
+
+    for (int i = 0; i < size; i++) {
+        numbers[i] = i + 1; 
+    }
+    printf("Tableau: \n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+    size++;
+    int* temp = realloc(numbers, size * sizeof(int));
+    if (temp == NULL) {
+        free(numbers); 
+        return 1;      
+    }
+    numbers = temp; 
+    numbers[5] = 6;
+
+    printf("Tableau après : \n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+    free(numbers);
+
+    return 0;
 }
